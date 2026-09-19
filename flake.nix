6cda45e22d2ab38327f535ec37d19fac4ff2ce13
Sam Kwort <skwort@skwort.dev>
2026-09-19 13:10:54 +1000

    chore: Add flake.nix
    Bootstrap environment.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "nx — update awareness for NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cargo
+            clippy
+            rustc
+            rustfmt
+          ];
+        };
+      });
+    };
+}
